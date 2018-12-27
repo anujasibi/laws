25ac91864b140dbc9601743c069fa5047881c937
anujasibi <45849232+anujasibi@users.noreply.github.com>
2018-12-27 17:45:13 +0530

    Update new.py

--- law/law/spiders/new.py
+++ law/law/spiders/new.py
@@ -19,7 +19,7 @@ class NewSpider(scrapy.Spider):
             yield scrapy.Request(url, callback=self.parse_new)
             
     def parse_new(self, response):
-        news=response.css('div.row a.v-serp-block-link::attr("href")').extract_first()
+        news=response.css('div.row a.v-serp-block-link::attr("href")').extract()
         for new in news:
             newu =response.urljoin(new)
             print(newu)
