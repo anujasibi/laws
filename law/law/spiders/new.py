b0b0a8ef3042801e30487096d7a4808bc60c3e4d
anujasibi <45849232+anujasibi@users.noreply.github.com>
2019-01-01 12:47:44 +0530

    Update new.py

--- law/law/spiders/new.py
+++ law/law/spiders/new.py
@@ -10,20 +10,33 @@ class NewSpider(scrapy.Spider):
     allowed_domains = ['www.avvo.com']
     start_urls = ['https://www.avvo.com/all-lawyers/ny/new_york.html']
    
+   #Extracting all practice areas   
     def parse(self, response):
-        links=response.css('div.row a.v-pa-list-link::attr("href")').extract()
-        for link in links: 
-            url=response.urljoin(link)
-            print(url)
-            
-            yield scrapy.Request(url, callback=self.parse_new)
+        next_url=response.css('.row.text-center.strong a::attr("href")').extract()
+        for url in next_url:
+            urls=response.urljoin(url)
+            print(urls)
+            yield scrapy.Request(urls, callback=self.parse_new)
             
     def parse_new(self, response):
+        areas= response.css('.row ul.link-list.u-vertical-padding-half li a::attr("href")').extract()
+        for area in areas:
+            areau=response.urljoin(area)
+            print(areau)
+            yield scrapy.Request(areau, callback=self.parse_lawyers)
+
+    def parse_lawyers(self,response):
         news=response.css('div.row a.v-serp-block-link::attr("href")').extract()
         for new in news:
             newu =response.urljoin(new)
             print(newu)
             yield scrapy.Request(newu, callback=self.parse_details)
+
+       #Follow the paginatin link
+        next_page_url = response.css('nav.pagination span a::attr("href")').extract_first()
+        if next_page_url is not None:
+            next_page_url=response.urljoin(next_page_url)
+            yield scrapy.Request(url=next_page_url, callback=self.parse_lawyers)
 
     def parse_details(self, response):
         
@@ -36,6 +49,9 @@ class NewSpider(scrapy.Spider):
                 'image':response.css('.row img::attr(src)').extract_first()
               
          }
+           
+
+           
             
            
     
